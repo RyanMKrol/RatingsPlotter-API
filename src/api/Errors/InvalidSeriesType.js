ff875bce30a881c71972f5904610444f064f1904
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-25 12:24:00 +0000

    Fix bug in InvalidSeriesType error
    * The stack trace was being applied to the wrong error
    type.

--- src/api/Errors/InvalidSeriesType.js
+++ src/api/Errors/InvalidSeriesType.js
@@ -5,7 +5,7 @@ export default class InvalidSeriesType extends Error {
 
     // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, SeriesNotFound)
+      Error.captureStackTrace(this, InvalidSeriesType)
     }
 
     this.Error = 'InvalidSeriesType'
