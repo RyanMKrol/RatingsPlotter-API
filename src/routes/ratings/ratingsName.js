83b00ef7807e71ccad45fee03b751c210761a511
Ryan Krol <ryankr@hotmail.co.uk>
2020-09-09 23:24:19 +0100

    Add cache to rating route responses

--- src/routes/ratings/ratingsName.js
+++ src/routes/ratings/ratingsName.js
@@ -1,44 +1,52 @@
 import express from 'express'
+
 import {
   fetchSeriesDataByName,
   getSeriesLinks,
-  getSeriesRatings,
+  getSeriesRatings
 } from './../../api'
+import { seriesRatingsCacheKey, getRatingsCache } from './common'
 
 var router = express.Router()
 
-router.use('/', async (req, res, next) => {
-  console.log("Using some custom middleware!")
-  next()
-})
-
 router.get('/:series', async (req, res, next) => {
   try {
-    const seriesData = await fetchSeriesDataByName(req.params.series)
+    const cache = getRatingsCache()
+
+    const seriesData = await cache.processItem(
+      `SeriesData - ${req.params.series}`,
+      async () => await fetchSeriesDataByName(req.params.series)
+    )
     const seriesId = seriesData.imdbID
     const numSeries = seriesData.totalSeasons
     const seriesTitle = seriesData.Title
     const poster = seriesData.Poster
-
     const seriesLinks = getSeriesLinks(numSeries, seriesId)
 
-    const seriesRatings = await Promise.all(seriesLinks.map(async (series) => {
-      return getSeriesRatings(series)
-    }))
+    const seriesRatings = await cache.processItem(
+      seriesRatingsCacheKey(seriesId),
+      async () => {
+        return await Promise.all(
+          seriesLinks.map(async series => {
+            return getSeriesRatings(series)
+          })
+        )
+      }
+    )
 
-    const filteredRatings = seriesRatings.filter((ratings) => ratings.length > 0)
+    const filteredRatings = seriesRatings.filter(ratings => ratings.length > 0)
 
     const ratingsMap = filteredRatings.reduce((acc, ratings, index) => {
-      acc[`series_${index+1}`] = ratings
+      acc[`series_${index + 1}`] = ratings
       return acc
     }, {})
 
     const response = Object.assign({}, seriesData, {
-      ratings: ratingsMap,
+      ratings: ratingsMap
     })
 
     res.send(response)
-  } catch(error) {
+  } catch (error) {
     res.status(error.StatusCode).send(error)
   }
 })
